797635d06bd2d8fb2db381bfbc32c06e09c20ee9
Kevinloritsch <kloritsch58@gmail.com>
2022-06-25 17:28:07 -0700

    Model Set, Show All

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,14 +7,21 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    @user.save
-    redirect_to '/thank'
+    if @user.save
+      redirect_to '/thank'
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @users = User.all
   end
 
 
     private
     def user_params
-      params.require(:user).permit(:first_name)
+      params.require(:user).permit(:first_name, :last_name, :birthday)
     end
   end
     
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'users#new'
   get '/new', to: 'users#new'
+  get '/users', to: 'users#new'
+
 
   get '/thank', to: 'users#thank'
   resources :users
