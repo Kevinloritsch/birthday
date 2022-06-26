55dfb756fcaf8400fcba3c4c2f8f84f50f151da2
Kevinloritsch <kloritsch58@gmail.com>
2022-06-26 16:17:12 -0700

    Next Bday Attempt 1

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
     validates :first_name, presence: true, length: { maximum: 75 }
     validates :last_name, presence: true, length: { maximum: 75 }
-    VALID_DATE_REGEX = /(?<month>\d{1,2})\/(?<day>\d{1,2})/
-    validates :birthday, presence: true, length: { maximum: 5, minimum: 5 }, format: { with: VALID_DATE_REGEX }
+    VALID_DATE_REGEX = /(?<month>\d{2,2})\/(?<day>\d{2,2})/
+    validates :birthday, presence: true, format: { with: VALID_DATE_REGEX }
 
 end
