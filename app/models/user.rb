926a7c5739e3ef028b83d4c154a2f4f49ef9b72e
Kevinloritsch <kloritsch58@gmail.com>
2022-07-04 15:35:31 -0700

    Start Frontend

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,5 @@ class User < ApplicationRecord
     validates :first_name, presence: true, length: { maximum: 75 }
     validates :last_name, presence: true, length: { maximum: 75 }
     VALID_DATE_REGEX = /(?<month>\d{2,2})\/(?<day>\d{2,2})/
-    validates :birthday, presence: true, format: { with: VALID_DATE_REGEX }
-
+    validates :birthday, presence: true, format: { with: VALID_DATE_REGEX }, length: { maximum: 5,minimum: 5 }
 end
