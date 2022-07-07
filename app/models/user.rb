4ec574d48ad099954bf6b5cc12167aea32ff9f4b
Kevinloritsch <kloritsch58@gmail.com>
2022-07-06 22:03:31 -0700

    Remove Short

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
     validates :first_name, presence: true, length: { maximum: 75 }
     validates :last_name, presence: true, length: { maximum: 75 }
     VALID_DATE_REGEX = /\A((0?1?1){1}|(0?1?2){1}|([0]?3|4|5|6|7|8|9))\/((0?1?2?3?1){1}|(0?1?2?(2|3|4|5|6|7|8|9|0))|(30))\z/
-    validates :birthday, presence: true, format: { with: VALID_DATE_REGEX }, length: { maximum: 5,minimum: 5 }
+    validates :birthday, presence: true, format: { with: VALID_DATE_REGEX }, length: { maximum: 5 }
 end
