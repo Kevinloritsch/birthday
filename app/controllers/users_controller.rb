5c49a1a6c143793408a1a98df03e275020ac2357
Kevinloritsch <kloritsch58@gmail.com>
2022-07-06 20:40:45 -0700

    Front End

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to '/thank'
+      flash[:success] = "Account Registered!"
+      redirect_to '/'
     else
       render 'new'
     end
